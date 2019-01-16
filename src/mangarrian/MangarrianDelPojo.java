43db872d1985c4bcae98a26f207a23907a68d014
21855113 <21855113@N205-10.alumnos.uem.es>
2019-01-16 11:16:21 +0100

    Cambio en el código

--- src/mangarrian/MangarrianDelPojo.java
+++ src/mangarrian/MangarrianDelPojo.java
@@ -4,6 +4,7 @@ public class MangarrianDelPojo {
 
 	public static void main(String[] args) {
 		System.out.println("Introduce tus pojos");
+		System.out.println("Dámelo todo y mete más pojos.");
 
 	}
 
