ac6f5bb4ef11e5dc3f6dbf31ab6eea7fb460f6c6
Lkjhgf25 <Lkjhgf25@DESKTOP-7D5DA5M>
2019-01-16 11:23:32 +0100

    modificacion

--- src/mangarrian/MangarrianDelPojo.java
+++ src/mangarrian/MangarrianDelPojo.java
@@ -5,7 +5,7 @@ public class MangarrianDelPojo {
 	public static void main(String[] args) {
 		System.out.println("Introduce tus pojos");
 		System.out.println("Dámelo todo y mete más pojos.");
-
+		System.out.println("El rey del pojo");
 	}
 
 }
